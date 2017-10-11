dfbbd35f726577d4a617f3b68ad5bc5b847c6dd7
parshencev <parshencev.vlad@gmail.com>
2017-10-11 16:23:21 +0300

    отказ от идеи

--- widget.js
+++ widget.js
@@ -33,7 +33,7 @@ var AstralWidget = function(){
 	this.events = {
 		open: (function(){
 			this.dom.widgetWrapper.classList.remove("widget_wrapper_close");
-			if(!this.token || this.token == "null" || this.subject != localStorage.getItem('widgetType')){
+			if(!this.token || this.token == "null"){
 				this.socket.emit('WIDGET_GET_TOKEN', {subject: this.subject});
 			} else {
 				this.socket.emit('GET_SESSION_ID', this.token);
@@ -99,7 +99,6 @@ var AstralWidget = function(){
 		setToken: function(token){
 			this.token = token;
 			localStorage.setItem('widgetToken', this.token);
-			localStorage.setItem('widgetType', this.subject);
 			this.socket.emit('GET_SESSION_ID', this.token);
 		},
 		setSessionId: function(data){
